c7ce39760dd4e322138691698375af36e8a6c72b
mieraldudhagara02 <118159123+mieraldudhagara02@users.noreply.github.com>
2024-08-06 15:26:43 +0530

    add all

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+const search = document.getElementById('input')
+const totalProducts = document.querySelectorAll('.products')
+
+const filterBySearch = () => {
+    const Value = search.value;
+
+    totalProducts.forEach(e => {
+        const productName = e.querySelector('.product-name').innerHTML
+        const productPrice = e.querySelector('.price-of-product').innerHTML
+
+        if (productName.includes(Value) || productPrice.includes(Value)) {
+            e.style.display = 'block' 
+        }
+        else 
+        {
+            e.style.display = 'none' 
+        }
+    });
+};
+search.addEventListener('input', filterBySearch)
+
 const sortList = (category, event) => {
     const list = {
         'all': 'All Items',
         'sneakers': 'Sneakers',
-        'wallet': 'Wallets',
+        'wallet': 'Wallet',
         'cap': 'Cap'
     };
 
@@ -14,15 +35,22 @@ const sortList = (category, event) => {
         }
 
         el.classList.remove('active-navigation-bar')
-    })
+    });
 
-    document.querySelectorAll('.products').forEach((el) => {
-        if (category === 'all' || el.getAttribute('data-category') === category) {
-            el.style.display = 'block'
-        } else {
+    totalProducts.forEach((el) => {
+        if (category === 'all' || el.getAttribute('data-category') === category) 
+        {
+            el.style.display ='block'
+        } 
+        else 
+        {
             el.style.display = 'none'
-        }
-    })
-}
+        }         
+    });
 
-sortList('all')
+    localStorage.setItem('selectedCategory', category)
+    
+};
+
+const savedCategory = localStorage.getItem('selectedCategory') || 'all'
+sortList(savedCategory)
